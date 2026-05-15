0950cc2349ffa0df82ce90d862bd0f06e8e46edb
Timos <psychoson91@gmail.com>
2026-05-15 18:24:28 +0300

    feat: Add ArrowFunctionalComponentWithProps component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
-import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
-import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
-import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+ //import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
+ //import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+ //import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 
-function App() {
+// type Props = {
+//     title: string,
+//     description: string,
+// }
+//
+// type PropsA = {
+//     title: string,
+//     description: string,
+// }
+//
+// interface PropsB {
+//     title: string,
+//     description: string,
+// }
+//
+// // UNION
+// type Status = "Error" | "Info" | "Warning";
+//
+// // Tuples
+// type Coordinates = [number, number];
+//
+// // Functions
+// type ClickHandler = (event: MouseEvent) => void;
+//
+// //Intersection
+// type Props = PropsA & PropsB;
+
+
+
+import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
+
+ function App() {
 
   return (
     <>
-      <FunctionalComponent />
-      <ClassComponent />
-      <ArrowFunctionalComponent />
+      {/*{LESSON THREE}*/}
+      {/*<FunctionalComponent />*/}
+      {/*<FunctionalComponent />*/}
+      {/*<FunctionalComponent />*/}
+      {/*<FunctionalComponent />*/}
+      {/*<ClassComponent />*/}
+      {/*<ArrowFunctionalComponent />*/}
+
+
+      {/*{Lesson Three} */}
+      <ArrowFunctionalComponentWithProps title="Heading 1"/>
+      <ArrowFunctionalComponentWithProps
+           title="Second Title"
+           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur fugit, inventore odio quia quibusdam quis ratione rem."
+       />
+      <ArrowFunctionalComponentWithProps title="Heading 1"/>
+
     </>
   )
 }
